0c6c42d1e8e924e1a24afaa3c242ae13540319d0
jerrytc7 <jerrykondner@gmail.com>
2021-11-22 12:03:42 -0500

    working-on-errors

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,37 +1,47 @@
 class StudentsController < ApplicationController
+    before_action :set_student, only: [:show, :update, :destroy]
+
     def index
         students = Student.all
         render json: students
     end
 
     def show
-        student = Student.find_by(id: params[:id])
-        render json: student
+        if @student
+            render json: student, status: :ok
+        else
+            render json: {error: "Student Not Found"}, status: :not_found
+        end
     end
 
     def create
         student = Student.create(student_params)
-        render json: student, status: :created
+        if student.valid?
+            render json: student, status: :created
+        else
+            render json: {error: student.errors.full_messages}, status: :unprocessable_entity
+        end
     end
 
     def update
-        student = Student.find_by(id: params[:id])
-        student.update(student_params)
+        @student.update(student_params)
         render json: student
     rescue ActiveRecord::RecordInvalid => invalid
-        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
-    
+        render json: { errors: errors.record.errors.full_messages }, status: :unprocessable_entity
     end
 
     def destroy
-        student = Student.find_by(id: params[:id])
-        student.destroy
+        @student.destroy
         head :no_content
     end
 
     private
 
     def student_params
-        params.permit(:firstname, :lastname, :grade)
+        params.permit(:firstname, :lastname, :grade, :id, :student)
+    end
+
+    def set_student
+        @student = Student.find_by(id: params[:id])
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(:username, :password, :password_confirmation, :user)
+        params.permit(:username, :password, :password_confirmation)
     end
 end
